de908275fafc642bc6d496578bc486df48bdc76c
eladraz49 <eladraz2008@gmail.com>
2024-11-21 00:02:34 +0200

    initial commit

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.alertdialog;
+
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btn = findViewById(R.id.btn);
+        EditText et = findViewById(R.id.et);
+        TextView tv = findViewById(R.id.tv);
+        SharedPreferences sharedPreferences = getSharedPreferences("alert", MODE_PRIVATE);
+        tv.setText(sharedPreferences.getString("key", "waiting for input"));
+        btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Wow")
+                    .setMessage("Awesome")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            String name = et.getText().toString();
+                            editor.putString("key", name);
+                            editor.apply();
+                            Toast.makeText(MainActivity.this,"you wrote " + name, Toast.LENGTH_LONG).show();
+
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+
+
+
+        }
+    });
+    }
+}
